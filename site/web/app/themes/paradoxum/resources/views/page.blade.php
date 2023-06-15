648b924b81a5b3c3e73dadbbf9bbe01b8ab9d09d
Daniel Semblano <dsemblano@gmail.com>
2023-06-15 07:05:54 -0300

    Block plugins, some css adjustments, no title on book author pages

--- site/web/app/themes/paradoxum/resources/views/page.blade.php
+++ site/web/app/themes/paradoxum/resources/views/page.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
+  @if ( !wp_get_post_parent_id( get_the_ID() ) != 0 )
+  @include('partials.page-header')
+  @endif
     @includeFirst(['partials.content-page', 'partials.content'])
   @endwhile
 @endsection
